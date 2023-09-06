2f8a9cf45b4511678cd9009ee4dbe160ab533f18
Andres Abarca Valenzuela <avalenzuela.andres@gmail.com>
2023-09-06 15:24:12 -0300

    First commit

---
+++ app/src/main/java/com/erbyte/androidmaster/Variables.kt
@@ -0,0 +1,39 @@
+package com.erbyte.androidmaster
+
+
+fun main() {
+    /**
+     * Variables numericas
+     */
+
+    // Int -2,147,483,648 to 2,147,483,647
+    val intExample:Int = 26 // Val cannot be reassigned
+    var intExample2:Int = 26
+
+    // Long -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+    val longExample:Long = 26
+
+    // Float 7 decimal digits
+    val floatExample:Float = 30.5f
+
+    // Double 14 decimal digits
+    val doubleExample:Double = 30.5
+
+    /**
+     * Variables alfanumericas
+     */
+
+    // Char 1 character
+    val charExample:Char = 'A'
+
+    // String 1 or more characters
+    val stringExample:String = "Hello World"
+
+    /**
+     * Variables booleanas
+     */
+
+    // Boolean true or false
+    val booleanExample:Boolean = true
+
+}
